8d892b727c2e42f147c64ed628218117f14fd877
Anthony Grimes <i@raynes.me>
2013-01-29 15:58:17 -0600

    Update laser and postal and add :main.

--- project.clj
+++ project.clj
@@ -5,9 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [tentacles "0.2.4"] 
-                 [me.raynes/laser "0.1.20"]
+                 [me.raynes/laser "0.1.21"]
                  [me.raynes/cegdown "0.1.0"]
                  [me.raynes/conch "0.5.0"]
-                 [com.draines/postal "1.9.1"]
+                 [com.draines/postal "1.9.2"]
                  [org.clojure/tools.cli "0.2.2"]
-                 [me.raynes/fs "1.4.0"]])
+                 [me.raynes/fs "1.4.0"]]
+  :main me.raynes.updoc)
